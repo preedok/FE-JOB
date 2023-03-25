717e75f7beaaf4e0f0bc89bc7dbefd57c70acdf0
muhamad iqbal apredo <edo.vivoy21@gmail.com>
2023-03-25 19:52:30 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
 import Script from "next/script";
-import store from "../redux/store";
-import { Provider } from "react-redux";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
@@ -20,9 +18,9 @@ function MyApp({ Component, pageProps }) {
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
         crossorigin="anonymous"
       />
-      <Provider store={store}>
+ 
         <Component {...pageProps} />
-      </Provider>
+    
     </>
   );
 }
