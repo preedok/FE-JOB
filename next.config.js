09025b3773c9c435030972b3c026a272f8515f56
muhamad iqbal apredo <edo.vivoy21@gmail.com>
2023-03-25 20:49:33 +0700

    update

--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ const nextConfig = {
     ]
   },
   images: {
-    domains: [ 'res.cloudinary.com', 'http://res.cloudinary.com/', 'http://localhost:4000', 'localhost']
+    domains: ['https://portalkerja2023.vercel.app', 'res.cloudinary.com', 'http://res.cloudinary.com/', 'http://localhost:4000', 'localhost']
   },
 }
 
